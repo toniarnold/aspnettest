e7250cf40c53b0c5b428b506104b7bb790f6d747
toniarnold <toni__arnold@bluewin.ch>
2022-02-10 14:33:08 +0100

    Add TestButton Blazor Component

---
+++ src/asplib.blazor/Components/TestButton.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace asplib.Components
+{
+    public partial class TestButtonBase : ComponentBase
+    {
+        [Parameter]
+        public string src { get; set; } = "/_content/asplib.blazor/nunit.png";
+
+        [Parameter]
+        public string TestResult { get; set; } = "";
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
