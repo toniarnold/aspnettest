79f3d4a2e28a1133a84417b6148387af890d1099
toniarnold <toni__arnold@bluewin.ch>
2022-07-24 13:47:13 +0200

    Amend defensive DatabaseKeyAttributeTest

---
+++ src/test.core/asplib/Model/DatabaseKeyAttributeTest.cs
@@ -0,0 +1,50 @@
+﻿using asplib.Model;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace test.asplib.Model
+{
+    public class DatabaseKeyAttributeTest
+    {
+        [Test]
+        public void ReadWriteProtectedMemory()
+        {
+            var secret = new byte[] { 1, 2, 3 };
+            AssertGetKey(secret);
+        }
+
+        private void AssertGetKey(byte[] secret)
+        {
+            var attr = new DatabaseKeyAttribute(secret);    // secret stored encrypted
+            Assert.That(attr.Key, Is.EqualTo(secret));      // retrieved from encrypted memory
+        }
+
+        [Test]
+        public void ConcurrentlyReadWriteProtectedMemory()
+        {
+            const int THREADS = 100;
+
+            // 1. create THREADS byte arrays
+            var sectrets = new List<byte[]>();
+            using (var aes = Aes.Create())
+            {
+                for (int i = 0; i < THREADS; i++)
+                {
+                    aes.GenerateKey();
+                    sectrets.Add(aes.Key);
+                }
+            }
+
+            // 2. write/read them in parallel
+            //    (might eventually detect thread safety problems with the static IDataProtector instance)
+            var tasks = new List<Task>();
+            foreach (var secret in sectrets)
+            {
+                tasks.Add(Task.Run(() => AssertGetKey(secret)));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
